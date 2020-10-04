bf529dfe1d14cd7605dcb7b86eb346342d1add2e
zesha5816 <71140785+zesha5816@users.noreply.github.com>
2020-10-04 14:23:09 -0400

    Update Main.java
    finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Write a program that will prompt the user to enter in a positive integer. It will then    calculate the sum from 1 up to that number.
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//prompt user for their integer
+    System.out.println("Please enter an integer to count down to");
+		//initializing variables
+		int num = input.nextInt(); 
+		int count = 1;
+		int total = 0;
+
+       while(count <= num){
+				 //adding a accumulator
+           total = total + count;
+					 //count = count + 1/count++;
+           count=count + 1;
+       }
+       System.out.println("The sum from 1 - " + num + " is " + total);
   }
 }
+
